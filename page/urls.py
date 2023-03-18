4bda945ec2c20cae166c4ba72b609d192f44dc74
Alice Kamuyu <alicekamuyu385@gmail.com>
2023-03-19 02:43:40 +0300

    html Django

---
+++ page/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import HomePageView, AboutpageView
+
+urlpatterns = [
+    path("", HomePageView.as_view(), name="home"),
+    path("about/", AboutpageView.as_view(), name='abouts')
+]
